a0361df80b45f8a83a2a52a45ae31ea664b0e59c
Basavaprabhu <prabhu26ec@gmail.com>
2026-02-15 16:23:07 +0530

    Add files via upload

---
+++ ABS_Simulink_Modelling/ABS.m
@@ -0,0 +1,10 @@
+
+% ENTER THESE VALUES IN COMMAND WINDOW 
+
+g=9.8100;    % Standard Earth gravity
+I=5;         % moment of inertia of the wheel
+m=75;        % 1/4 of vehicle mass 
+R=1.2500;    % wheel radius
+v0=22.22;    % initial vehicle velocity (Vehicle speed = 80 km/h)
+
+
